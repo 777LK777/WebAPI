f3dc514593621805ce937c4267fdcf1b8ce68e0f
Леонид <43683961+777LK777@users.noreply.github.com>
2019-01-20 04:33:53 +0300

    Выгрузка

---
+++ WebApp/WebApp/Controllers/SendController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class SendController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SomeResult()
+        {
+            MsgData messageData = new MsgData();
+            return View(messageData);
+        }
+
+        
+        [HttpPost]
+        public ActionResult SomeResult([FromForm] MsgData messageData)
+        {           
+
+            using (var client = new WebClient())
+            {
+                var parameters = new NameValueCollection
+                {
+                    { "token", messageData.AppToken },
+                    { "user", messageData.UserKey },
+                    { "message", messageData.Message }
+                };
+
+                client.UploadValues("https://api.pushover.net/1/messages.json", parameters);
+            }           
+
+            return Redirect("~/");            
+        }               
+    }
+}
---
+++ WebApp/WebApp/Models/MsgData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class MsgData
+    {
+        public string AppToken { get; set; }
+        public string UserKey { get; set; }
+        public string Message { get; set; }
+    }
+}
